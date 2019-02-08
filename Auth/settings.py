b2edcefede5524fca0cc273d51631a13d2cb1499
yakovlev <ivanvyakovlev@gmail.com>
2019-02-08 19:14:14 +0300

    secret key env

--- Auth/settings.py
+++ Auth/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@w7w^=@#0%fi!4^jspbr#g4w4*6-3junt8gz9+68=al+auzr6n'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
