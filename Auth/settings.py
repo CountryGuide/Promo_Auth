cbe930291f56728c44dddffade1be042e56e10fc
Ivan Yakovlev <ivanvyakovlev@gmail.com>
2019-02-09 22:44:57 +0300

    env settings

--- Auth/settings.py
+++ Auth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
-print(DEBUG)
+print(f'DEBUG={DEBUG}')
 
 ALLOWED_HOSTS = [
     'https://auth-ivanvyakovlev.herokuapp.com/'
