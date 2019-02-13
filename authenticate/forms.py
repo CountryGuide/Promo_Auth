135e0f5f3c5ac3cd5fa10d855d0bcc997bbe668f
yakovlev <ivanvyakovlev@gmail.com>
2019-02-13 18:44:07 +0300

    WIP registration/login/logout

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -55,3 +55,10 @@ class ChangePasswordForm(PasswordChangeForm):
         self.fields['old_password'].widget.attrs['class'] = 'form-control'
         self.fields['new_password1'].widget.attrs['class'] = 'form-control'
         self.fields['new_password2'].widget.attrs['class'] = 'form-control'
+        self.fields['new_password1'].help_text = '<ul class="form-text text-muted small">' \
+                                                 '<li>Your password can\'t be too similar to your other ' \
+                                                 'personal information.</li>' \
+                                                 '<li>Your password must contain at least 8 characters.</li>' \
+                                                 '<li>Your password can\'t be a commonly used password.</li>' \
+                                                 '<li>Your password can\'t be entirely numeric.</li>' \
+                                                 '</ul>'
